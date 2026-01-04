9f996286575ba9784bde274245d4bad599071103
AtharvBolke99 <atharvbolke99@gmail.com>
2026-01-04 15:58:36 +0530

    navbar fixed with minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,34 @@ import { Link } from "react-router";
 
 function Navbar() {
   return (
-    <div className="m-1.5 flex justify-between items-center shadow-[0_0_7px_rgb(145,231,145)] p-0 px-3.5 rounded-full bg-white sticky top-2 h-[60px] max-w-full z-10">
+    <div className="m-1.5 flex justify-between items-center shadow-[0_0_7px_rgb(145,231,145)] p-0 px-3.5 rounded-full bg-white sticky top-2 h-15 max-w-full z-10">
       <h2 className="text-xl font-semibold">
         Veggie<span className="text-green-300">Mart</span>
       </h2>
       <div className="flex ">
-        <Link to="/" className="text-xl m-2.5 no-underline border-b--2-transparent text-black hover:border-b-2 hover:border-green-300 hover:text-green-300 transition duration-300">
+        <Link
+          to="/"
+          className="text-xl m-2.5 no-underline border-b--2-transparent text-black hover:border-b-2 hover:border-green-300 hover:text-green-300 transition duration-300"
+        >
           Home
         </Link>
-        <Link to="/vegetables" className="text-xl m-2.5 no-underline text-black hover:border-b-2 hover:border-green-300 hover:text-green-300 transition duration-300">
+        <Link
+          to="/vegetables"
+          className="text-xl m-2.5 no-underline text-black hover:border-b-2 hover:border-green-300 hover:text-green-300 transition duration-300"
+        >
           Vegetable
         </Link>
-        <Link to="/contact" className="text-xl m-2.5 no-underline text-black hover:border-b-2 hover:border-green-300 hover:text-green-300 transition duration-300 hidden md:block">
+        <Link
+          to="/contact"
+          className="text-xl m-2.5 no-underline text-black hover:border-b-2 hover:border-green-300 hover:text-green-300 transition duration-300 hidden md:block"
+        >
           Contact Us
         </Link>
-        
-        <Link to="/cart" className="text-xl m-2.5 no-underline text-black hover:border-b-2 hover:border-green-300 hover:text-green-300 transition duration-300">
+
+        <Link
+          to="/cart"
+          className="text-xl m-2.5 no-underline text-black hover:border-b-2 hover:border-green-300 hover:text-green-300 transition duration-300"
+        >
           Cart
         </Link>
       </div>
